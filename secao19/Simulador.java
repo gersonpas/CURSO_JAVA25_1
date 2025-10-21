53e46ade6909721eb637dd38b18f0cbcb61e2bcd
gersonpas <sobrinho.gerson@gmail.com>
2025-10-20 23:50:06 -0300

    Atualização secao 19

--- secao19/Simulador.java
+++ secao19/Simulador.java
@@ -4,46 +4,53 @@ import java.util.Scanner;
 
 public class Simulador {
     
-    public static void main(String[] args){
+     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         ArrayList<Bilhete> bilhetes = new ArrayList<>();
-        
-        // Solicitando bilhetes de usuário
-            System.out.println("Digite 6 números para o seu bilhete(entre 1 e 60: )");
-                    int[] numerosEcolhidos = new int[6];
-                        
-                    // Usuário escolhe os números 
-                for(int i = 0; i < numerosEcolhidos.length; i++){
-                        System.out.println("Digite o número " + (i + 1) + ": ");
-                        numerosEcolhidos[i] = scanner.nextInt();                         
 
+        // Solicitando bilhetes do usuário
+        while(true) {
 
-                }
-                    // Criamos o bilhete com os números escolhidos
-                    Bilhete bilhete = new Bilhete(numerosEcolhidos);
-                    bilhetes.add(bilhete);
+            System.out.println("Digite 6 números para o seu bilhete(entre 1 e 60: ");
 
-               // Pergunta se o usuário quer continuar     
-            System.out.println("Deseja registrar outro bilhete? (s/n)");
-                    String resposta = scanner.next();
-                   
-                    
-            while (true) {
-                    if (resposta.equals("n")) {
-                        break;
-                        
-                    }                
-                    // Realizar o sorteio para cada bilhete
-                    System.out.println("Realizando o sorteio...");
-                    
-                      for (Bilhete bilhete2 : bilhetes) {
-                        bilhete.realizarSorteio();
-                        bilhete.ExibirResultado();
-                        break;
-                      }
-                        scanner.close(); 
-                        
-                }                  
-            } 
-        
-    }       
+            int[] numerosEscolhidos = new int[6];
+
+            // Usuario escolhe numeros
+            for(int i = 0; i < numerosEscolhidos.length; i++) {
+
+                System.out.println("Digite o número " + (i + 1) + ": ");
+
+                numerosEscolhidos[i] = scanner.nextInt();
+                
+            }
+
+            // Criamos o bilhete com os numeros escolhidos
+            Bilhete bilhete = new Bilhete(numerosEscolhidos);
+
+            bilhetes.add(bilhete);
+
+            // Pergunta se o usuario quer continuar
+            System.out.println("Deseja registrar outro bilhete? (s / n)");
+
+            String resposta = scanner.next();
+
+            if(resposta.equals("n")) {
+                break;
+            }
+
+        }
+
+        // Realizar o sorteio para cada bilhete
+        System.out.println("Realizando o sorteio...");
+
+        for(Bilhete bilhete : bilhetes) {
+            bilhete.realizarSorteio();
+            bilhete.ExibirResultado();
+        }
+
+        scanner.close();
+
+    }
+
+}
