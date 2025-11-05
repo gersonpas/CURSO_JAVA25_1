26f79cb2c9ee40251e751e937cff42e945488910
gersonpas <sobrinho.gerson@gmail.com>
2025-11-04 23:33:39 -0300

    Atualização Exercicios Seção14

---
+++ secao20/Exercicios/Exercicios.java
@@ -0,0 +1,14 @@
+package secao20.Exercicios;
+
+public class Exercicios {
+
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco("Rua Nova", 0125, "Brasília - DF");
+        Pessoa pessoa = new Pessoa("Fulano ", 35, endereco);
+        
+        pessoa.exibirInfo();
+        // System.out.println(endereco.getEnderecoCompleto());
+
+    }
+    
+}
--- secao20/README.md
+++
@@ -1,2 +0,0 @@
-Programas escritos em Java do curso da Udemy ministradas pelo Professor
-Matheus Batisti em Outubro de 2025.
