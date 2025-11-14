513378fdabd8ac4b8d9d62db8ba33b0fd44b872b
gersonpas <sobrinho.gerson@gmail.com>
2025-11-14 00:46:41 -0300

    Seção21 -> Tratamento de erros try catch

--- secao21/Erros.java
+++ secao21/Erros.java
@@ -1,5 +1,20 @@
 package secao21;
 
 public class Erros {
+
+    public static void main(String[] args) {
+        
+        // 1 - try catch
+        try{
+            int a = 0;
+            int b = 10;
+            int resultado = b/a;
+
+
+        } catch (ArithmeticException e) {
+            System.out.println("Divisão por zero não é possível.");
+        } 
+
+    }
     
 }
