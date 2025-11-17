252a99c1b33e5c55378b53f2ed17396839085610
gersonpas <sobrinho.gerson@gmail.com>
2025-11-16 21:00:21 -0300

    Seção22 ->  Escrita em  arq. e erros

--- secao22/Arquivo.java
+++ secao22/Arquivo.java
@@ -8,7 +8,11 @@ public class Arquivo {
         
 
         // 1 - Leitura de arquivos.
+         // FileReader = caracter por caracter
+        // BufferedReader = linha por linha
 
+
+        // Ler o arquivo linha por linha.
         String currentDir = System.getProperty("user.dir") + "\\secao22\\";
         System.out.println("");
         System.out.println(currentDir);
@@ -25,10 +29,50 @@ public class Arquivo {
             
         }catch (Exception e) {
             System.out.println("Erro ao ler arquivo." + e.getMessage());
+        }
+
+        System.out.println();
+
+
+        // Ler o arquivo linha por linha.
+        System.out.println("Lendo o arquivo linha por linha.");
+        try (BufferedReader reader = new BufferedReader(new FileReader(currentDir + "arquivo.txt"))) {
+            
+            String linha;
+
+            while((linha = reader.readLine()) != null) {
+                System.out.println(linha);
+            }
+
+        } catch (Exception e) {
+           
+            System.out.println("Erro ao ler arquivo: " + e.getMessage());
 
         }
 
+        // 2 => Escrever em arquivos
+
+        try (FileWriter writer = new FileWriter(currentDir + "saida.txt")){
+
+            writer.write("Escrevendo em arquivo \n");
+            writer.write("Escrevendo em arquivo 2");
+            
+        } catch (Exception e) {
+            System.out.println("Erro ao escrever em arquivo: " + e.getMessage());           
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentDir + "saida2.txt"))){
+            
+            writer.write("Escrevendo em arquivo com o buffered");
+            writer.newLine();
+            writer.write("Escrevendo em arquivo com o buffered 2");
+            
+
+        }catch (Exception e) {
+
+            System.out.println("Erro ao escrever em arquivo: " + e.getMessage());           
+
+        }
     }
-
+   
 }
-
