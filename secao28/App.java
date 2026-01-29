c5cd075c6b4e52a342ac02ca674d585fd962df61
gersonpas <sobrinho.gerson@gmail.com>
2026-01-28 23:34:39 -0300

    Secao28 Tela Logim Scene Builder

--- secao28/App.java
+++ secao28/App.java
@@ -1,40 +1,30 @@
 package secao28;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args) throws Exception {
+        launch(args);
     }
 @Override
-public void start(Stage primaryStage) {
-    Button btn = new Button();
-    btn.setText("Diga 'Alô Mundo!'");
-    btn.setOnAction(new EventHandler<ActionEvent>() {
-        @Override
-        public void handle(ActionEvent event) {
-            System.out.println("Alô Mundo Java!");
-        }
-    });
+public void start(Stage primaryStage) throws Exception {
+   
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LayoutScBuild.fxml"));
+    Parent root = fxmlLoader.load();
 
-    StackPane root = new StackPane();
-    root.getChildren().add(btn);
+    Scene tela = new Scene(root);
 
-    Scene scene = new Scene(root, 300, 250);
-
-    primaryStage.setTitle("Olá Mundo Java!");
-    primaryStage.setScene(scene);
+    primaryStage.setScene(tela);
+    primaryStage.setTitle("Exemplo de Scene Builder");
     primaryStage.show();
-    
-    
 
+
+    
 }
 }
 
